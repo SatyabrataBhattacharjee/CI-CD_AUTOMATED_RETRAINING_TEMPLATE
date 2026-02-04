3cef38cb70fe3230424294948053c85fb1bc3413
SatyabrataBhattacharjee <satyabratbhattacharya1408@gmail.com>
2026-02-04 18:36:45 +0530

    Update init_db.py

--- src/ingestion/init_db.py
+++ src/ingestion/init_db.py
@@ -8,27 +8,20 @@ def init_database():
         # CUSTOMER 7-DAY SUMMARY TABLE
         # -----------------------------
         conn.execute(text("""
-            CREATE TABLE IF NOT EXISTS customer_7day_summary (
-         
+          CREATE TABLE IF NOT EXISTS customer_7day_summary (
 
-                -- Numerical (mean aggregated)
-                quantity FLOAT CHECK (quantity >= 0),
-                line_net_amount FLOAT CHECK (line_net_amount >= 0),
-                total_items FLOAT CHECK (total_items >= 0),
-                total_cost FLOAT CHECK (total_cost >= 0),
+    -- Numerical (mean aggregated)
+    quantity FLOAT CHECK (quantity >= 0),
+    line_net_amount FLOAT CHECK (line_net_amount >= 0),
+    total_items FLOAT CHECK (total_items >= 0),
+    total_cost FLOAT CHECK (total_cost >= 0),
 
-                -- Categorical (mode aggregated)
-               
-                
-                loyalty_status TEXT,
-             
-               
-                
-                payment_method TEXT,
-                discount_applied BOOLEAN,
+    -- Categorical (mode aggregated)
+    loyalty_status TEXT,
+    payment_method TEXT,
+    discount_applied BOOLEAN
+);
 
-
-            );
         """))
 
         # -----------------------------
